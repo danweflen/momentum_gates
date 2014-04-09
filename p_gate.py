e9b57be317fbfefe657b4ebb878ba4a824fd1db3
Daniel Weflen <weflen@weflen-thinkpad.(none)>
2014-04-09 14:36:13 -0600

    Added first code to actually read in the data file.

--- p_gate.py
+++ p_gate.py
@@ -6,11 +6,13 @@ import scipy.linalg as lin
 import numpy as np
 import npsf_interface as npsf
 import ionization_utilities as ion
+import pandas as pd
 import math
 from multiprocessing import Pool
 from string import *
 import sys, itertools, os
 import wigner
 
-wigner.plotExample()
-
+datafile_name=sys.argv[1]
+wfn_timeseries=pd.DataFrame(np.loadtxt(datafile_name).transpose())
+print wfn_timeseries
