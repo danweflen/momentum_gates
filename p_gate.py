6994c0ffa7c193f3a9ed46fbb80c45cd318fa244
Daniel Weflen <weflen@colorado.edu>
2014-04-14 16:58:49 -0600

    Untested version of p_gate.py that has been rewriten to produce animations.

--- p_gate.py
+++ p_gate.py
@@ -9,33 +9,37 @@ import ionization_utilities as ion
 import pandas as pd
 import math
 from multiprocessing import Pool
+from matplotlib import animation
 from string import *
 import sys, itertools, os
 import wigner
 
+#Reading in the wavefunction and initializing parameters that only depend on the
+#parameters of the wavefunction.
 datafile_name=sys.argv[1]
 store=pd.HDFStore(datafile_name)
 wfn_timeseries=store['wavefunction']
 wavefunction=np.array([wfn_timeseries[10][x/100.0] for x in range(-5000,5000)])
-del wfn_timeseries, store
-
-wigner_function=wigner.wdf(wavefunction)
 N=wavefunction.size
 t=sp.linspace(-50,50,N)
 ell = sp.asarray(range(0,N)) - N/2
 dt = t[1]-t[0]
-print "dt: ",dt
 s =  ell / (dt * N) 
-print "s: ",s
 T, S = sp.meshgrid(t,s)
 
-plt.figure()
-plt.contourf(T.transpose(), S.transpose(), wigner_function, cmap='jet')
-plt.xlim(-10,10)
-plt.ylim(-2,2)
-plt.colorbar()
-plt.xlabel('x', fontsize=16)
-plt.ylabel('p', fontsize=16)
-plt.legend()
-plt.show()
+#Creating the figure and setting the figure parameters.
+fig=plt.figure()
+ax=fig.add_subplot(111,autoscale_on=False, xlim=(-10,10), ylim=(-2,2))
 
+#Animate function: this is called sequentially by FuncAnimation
+def animate(i):
+    wavefunction=np.array([wfn_timeseries[10*i+10][x/100.0] for x in range(-5000,5000)])
+    wigner_function=wigner.wdf(wavefunction)
+    image=ax.contourf(T.transpose(), S.transpose(), wigner_function, cmap='jet')
+    return image
+
+#Animating and saving the resulting video.
+anim=animation.FuncAnimation(fig, animate, frames=300)
+anim.save("test_animation.mp4")
+
+plt.show()
