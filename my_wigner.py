561cdf324902bf83f952a3940393a19f01be538e
Daniel Weflen <weflen@colorado.edu>
2014-04-28 16:49:08 -0600

    Working version of my_wigner.py.

--- my_wigner.py
+++ my_wigner.py
@@ -5,22 +5,27 @@ import numpy as np
 
 def wigner_distribution(psi):
     #psi is assumed to be a numpy array
-    wigner=np.zeros((psi.size,psi.size))
-    for position in np.arange(0,psi.size):
+    if psi.size%2==0:
+        psi=np.pad(psi, (0,1),mode="constant")
+    npts=psi.size
+    wigner=sp.zeros((npts,npts), complex)
+    for position in np.arange(0,npts):
         psi_copy_low=psi
-        psi_copy_high=psi
-        psi_copy_low=np.roll(psi_copy_low,position)
-        psi_copy_low[0:position]=0.0
-        psi_copy_high=psi_copy_high[::-1]
-        psi_copy_high=np.roll(psi_copy_high,-position)
-        print "Matchup: "
-        print psi_copy_low
-        print psi_copy_high
-        if position != 0:
-            psi_copy_high[-position:]=0.0
-        acorr=psi_copy_low*np.conj(psi_copy_high)
+        shift=position-psi.size/2
+        psi_copy_low=np.roll(psi_copy_low,-shift)
+        if shift<0:
+            psi_copy_low[:-shift]=0.0
+        elif shift>0:
+            psi_copy_low[-shift:]=0.0
+        acorr=psi_copy_low*np.conj(psi_copy_low[::-1])
         ftrans=ft.fft(acorr,overwrite_x=True)
-        print "Position: ", position
-        ftrans=2/sp.pi*np.real(ftrans)
-        wigner[position]ftrans
-    return wigner
+        ftrans=1/sp.pi*ftrans
+        #Removing the linear phase induced by the fft function
+        #considering the first element as 0 rather than the
+        #middle element. This uses the fourier shifting theorem.
+        shift=ftrans.size/2+1
+        permuted_fft=ftrans*np.exp(-2j*math.pi*shift*np.arange(0,ftrans.size)/ftrans.size)
+        #Shifting the fft itself back into place.
+        ftrans=ft.fftshift(permuted_fft)
+        wigner[position]=ftrans
+    return wigner.transpose()
