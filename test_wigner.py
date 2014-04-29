b07aba1dce5ee111103d7c73e0bcee6e67ad702d
Daniel Weflen <weflen@weflen-thinkpad.(none)>
2014-04-29 16:47:54 -0600

    Stuff to check that the relative error only gets bit (~300) where the absolute error and the value of the function are small (~10^-50).

--- test_wigner.py
+++ test_wigner.py
@@ -5,7 +5,7 @@ import scipy as sp
 from my_wigner import wigner_distribution
 import matplotlib.pyplot as plt
 
-grid=np.arange(-10,10,0.05)
+grid=np.arange(-10,10.05,0.05)
 arg=np.fmax(np.exp(-(grid)**2),np.zeros(grid.size))
 x=np.arange(-10,10.05,0.05).reshape((1,grid.size))
 p=np.arange(-10,10.05,0.05).reshape((grid.size,1))
@@ -15,13 +15,21 @@ pgrid=np.ones(grid.size).reshape((1,grid.size))*p
 wig=wigner_distribution(arg)*0.05
 real=sp.real(wig)
 imag=sp.imag(wig)
-test=(real-ana)
+test=2*(ana-real)/(ana+real)
+print np.max(np.abs(test))
+print np.abs(test).mean()
+argmax=np.argmax(test)
+amax=np.amax(test)
+print amax
+print real.flat[argmax]
+print ana.flat[argmax]
 np.save("test.npy",test)
 grid.resize(wig.shape[1])
-plt.contourf(grid,grid,real,50,cmap="jet")
+plt.contourf(grid,grid,test,50,cmap="jet")
+plt.title("test")
 plt.colorbar()
-plt.xlim(-5,5)
-plt.ylim(-5,5)
+plt.xlim(-10,10)
+plt.ylim(-10,10)
 np.save("real.npy",real)
 plt.show()
 
