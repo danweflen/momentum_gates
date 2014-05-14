b76b159daa866f7d45dd6a4c3d4db7e7995457d3
Daniel Weflen <weflen@colorado.edu>
2014-05-13 20:51:14 -0600

    Updated p_gate.py to use my version of the wigner function.

--- p_gate.py
+++ p_gate.py
@@ -12,16 +12,17 @@ from multiprocessing import Pool
 from matplotlib import animation
 from string import *
 import sys, itertools, os
-import wigner
+from my_wigner import wigner_distribution_2
 
 #Reading in the wavefunction and initializing parameters that only depend on the
 #parameters of the wavefunction.
 datafile_name=sys.argv[1]
 store=pd.HDFStore(datafile_name)
 wfn_timeseries=store['wavefunction']
-wavefunction=wfn_timeseries[10].values
+times=filter(lambda x: x>1000 and x<2000, wfn_timeseries.columns)
+wavefunction=wfn_timeseries[times[0]].values
 N=wavefunction.size
-t=sp.linspace(-50,50,N)
+t=np.array(wfn_timeseries.index)
 ell = sp.asarray(range(0,N)) - N/2
 dt = t[1]-t[0]
 s =  ell / (dt * N)
@@ -32,18 +33,18 @@ del t,s
 fig=plt.figure()
 plt.xlabel("x")
 plt.ylabel("p")
-wigner_function=np.real(wigner.wdf(wavefunction))
+wigner_function=wigner_distribution_2(wavefunction)
 ax=fig.add_subplot(111,autoscale_on=False, xlim=(-10,10), ylim=(-1.5,1.5))
 wigner_figure =ax.contourf(T.transpose(), S.transpose(), wigner_function, cmap="jet")
 
 #Animate function: this is called sequentially by FuncAnimation
 def animate(i):
-    wigner_function=-1*wigner.wdf(wfn_timeseries[10*i+10].values)
+    wigner_function=wigner_distribution_2(wfn_timeseries[times[i]].values)
     wigner_figure=ax.contourf(T.transpose(), S.transpose(), wigner_function, cmap="RdBu")
     return wigner_figure
 
 #Animating and saving the resulting video.
-anim=animation.FuncAnimation(fig, animate, frames=260)
+anim=animation.FuncAnimation(fig, animate, frames=50)
 store.close()
-anim.save("/users/becker/weflen/momentum_gates/norio.mp4", fps=20)
+anim.save("/users/becker/weflen/momentum_gates/norio_dup.mp4", fps=10)
 
