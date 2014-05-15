8552c3066f4fbfe15bc2c93a835df833c94a8944
Daniel Weflen <weflen@colorado.edu>
2014-05-15 17:15:42 -0600

    Code to compare the two versions of the wigner function calculation.

--- test_wigner.py
+++ test_wigner.py
@@ -6,11 +6,11 @@ from my_wigner import wigner_distribution, wigner_distribution_2
 import matplotlib.pyplot as plt
 import time
 
-grid=np.arange(-10,10.05,0.05)
+grid=np.arange(-10,10.01,0.01)
 arg=np.fmax(np.exp(-(grid)**2),np.zeros(grid.size))
-x=np.arange(-10,10.05,0.05).reshape((1,grid.size))
-p=np.arange(-10,10.05,0.05).reshape((grid.size,1))
-ana=np.sqrt(1/(2*pi))*np.exp(-2*x**2)*np.exp(-pi**2*p**2/2)
+x=np.arange(-10,10.01,0.01).reshape((1,grid.size))
+p=np.arange(-10,10.01,0.01).reshape((grid.size,1))
+ana=np.sqrt(1/(2*pi))*np.exp(-2*x**2)*np.exp(-pi**2*p**2/2.0)
 xgrid=np.ones(grid.size).reshape((grid.size,1))*x
 pgrid=np.ones(grid.size).reshape((1,grid.size))*p
 start=time.time()
@@ -19,15 +19,15 @@ wig2=wigner_distribution_2(arg)*(grid[1]-grid[0])
 end=time.time()
 print "Wigner calculation time: ", end-start
 real=wig
-test=wig-wig2
+test=np.abs(wig2-ana)
 #test=2*(ana-real)/(ana+real)
 print "ana[0,0]: ", ana[0,0]
 print "real[0,0]: ", real[0,0]
-print "max absolute error: ",np.max(np.abs(test))
+print "max absolute value error: ",np.max(np.abs(test))
 print "mean absolute error: ", np.abs(test).mean()
 argmax=np.argmax(test)
 amax=np.amax(test)
-print "max absolute error", amax
+print "max error", amax
 print "real at max absolute error: ",real.flat[argmax]
 print "ana at max absolute error: ",ana.flat[argmax]
 np.save("test.npy",test)
