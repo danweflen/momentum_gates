71f622d9d7f884be3412142de781edcaf1ab3b03
Daniel Weflen <weflen@colorado.edu>
2014-05-23 13:10:51 -0600

    Checking old versions of the Wigner distribution.

--- my_wigner.py
+++ my_wigner.py
@@ -1,4 +1,4 @@
-from math import pi
+import math
 import scipy as sp
 import scipy.fftpack as ft
 import numpy as np
@@ -6,7 +6,7 @@ import numpy as np
 def wigner_distribution(psi):
     #psi is assumed to be a numpy array
     if psi.size%2==0:
-        raise ValueError
+        psi=np.pad(psi, (0,1),mode="constant")
     npts=psi.size
     wigner=sp.zeros((npts,npts), complex)
     for position in np.arange(0,npts):
@@ -17,38 +17,15 @@ def wigner_distribution(psi):
             psi_copy_low[:-shift]=0.0
         elif shift>0:
             psi_copy_low[-shift:]=0.0
-        acorr=np.conj(psi_copy_low)*psi_copy_low[::-1]
-        ftrans=ft.ifft(acorr)
-        #the fft is normalized, so if you want to evaluate the actual
-        #integral defined by the inverse fft, you have to multiply by
-        #the size of the transform.
-        ftrans=ftrans*ftrans.size/pi
+        acorr=psi_copy_low*np.conj(psi_copy_low[::-1])
+        ftrans=ft.fft(acorr,overwrite_x=True)
+        ftrans=1/sp.pi*ftrans
         #Removing the linear phase induced by the fft function
         #considering the first element as 0 rather than the
         #middle element. This uses the fourier shifting theorem.
         shift=ftrans.size/2+1
-        permuted_fft=ftrans*np.exp(-2j*pi*shift*np.arange(0,ftrans.size)/ftrans.size)
+        permuted_fft=ftrans*np.exp(-2j*math.pi*shift*np.arange(0,ftrans.size)/ftrans.size)
         #Shifting the fft itself back into place.
         ftrans=ft.fftshift(permuted_fft)
         wigner[position]=ftrans
-    print "Average Imaginary Part: ", np.average(np.imag(wigner))
-    print "Maximum Imaginary Part: ", np.max(np.abs(np.imag(wigner)))
-    wigner=np.real(wigner)
     return wigner.transpose()
-
-def wigner_distribution_2(psi):
-    npts=psi.size
-    wigner=sp.zeros((npts,npts))
-    for position in np.arange(0,npts):
-        width=min(position, npts-position)
-        up=psi[position:position+width]
-        down=psi[position-width+1:position+1]
-        down=down[::-1]
-        acorr=down*np.conj(up)
-        ftrans=np.fft.irfft(acorr,n=npts)
-        ftrans=ftrans*ftrans.size/pi
-        #Shifting the fft itself back into place.
-        ftrans=ft.fftshift(ftrans)
-        wigner[position]=ftrans
-    return wigner.transpose()
-
--- test_wigner.py
+++ test_wigner.py
@@ -1,55 +1,27 @@
 #! /usr/bin/env python
-from math import pi
 import numpy as np
 import scipy as sp
-from my_wigner import wigner_distribution, wigner_distribution_2
+from my_wigner import wigner_distribution
 import matplotlib.pyplot as plt
-import time
+from math import pi
 
 grid=np.arange(-10,10.05,0.05)
-N=grid.size
-t=grid
-ell = sp.asarray(range(0,N)) - N/2
-dx = t[1]-t[0]
-s =  2*ell / (dx * N) 
-T, S = sp.meshgrid(t,s)
-a=0.2
-k=-9
-arg=np.exp(-a*(grid)**2)*np.exp(1j*k*grid)
+arg=np.fmax(np.exp(-(grid)**2),np.zeros(grid.size))
 x=np.arange(-10,10.05,0.05).reshape((1,grid.size))
 p=np.arange(-10,10.05,0.05).reshape((grid.size,1))
-#The pi comes from the fact that the calculated and analytical grids
-#don't match up. The definition of the fourier transform has a pi
-#in the exponent, this means that p_ana=p_wig/pi. Currently I get
-#
-#
-ana=1/np.sqrt(2*pi)*np.exp(-2*a*x**2)*np.exp(-(pi*p-k)**2/(2*a))
+ana=np.sqrt(1/(2*pi))*np.exp(-2*x**2)*np.exp(-(p*pi)**2/2)
 xgrid=np.ones(grid.size).reshape((grid.size,1))*x
 pgrid=np.ones(grid.size).reshape((1,grid.size))*p
-start=time.time()
-wig=wigner_distribution(arg)*(grid[1]-grid[0])
-wig2=wigner_distribution_2(arg)*(grid[1]-grid[0])/2.0
-end=time.time()
-print "Wigner calculation time: ", end-start
-real=wig2
-test=real-ana
-#test=2*(ana-wig)/(ana+wig)
-print "ana[0,0]: ", ana[0,0]
-print "real[0,0]: ", real[0,0]
-print "max absolute value error: ",np.max(np.abs(test))
-print "mean absolute error: ", np.abs(test).mean()
-argmax=np.argmax(test)
-amax=np.amax(test)
-print "max error", amax
-print "real at max absolute error: ",real.flat[argmax]
-print "ana at max absolute error: ",ana.flat[argmax]
+wig=wigner_distribution(arg)*0.05
+real=sp.real(wig)
+imag=sp.imag(wig)
+test=(real-ana)
 np.save("test.npy",test)
 grid.resize(wig.shape[1])
-plt.contourf(T,S,test,50,cmap="jet")
-plt.title("test")
+plt.contourf(grid,grid,test,50,cmap="jet")
 plt.colorbar()
-plt.xlim(-10,10)
-plt.ylim(-10,10)
+plt.xlim(-5,5)
+plt.ylim(-5,5)
 np.save("real.npy",real)
-plt.savefig("test.png")
 plt.show()
+
